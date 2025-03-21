6d9df94daffe7e95d1f1546dc65351b18e7fbc74
Delgermaa12 <odelgermaa18@gmail.com>
2025-03-21 15:33:28 +0800

    Zassaan

--- Java/src/test/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntListTest.java
+++ Java/src/test/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntListTest.java
@@ -4,26 +4,17 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
-/**
- * Tests for the DelegationSortedIntList class.
- */
 public class DelegationSortedIntListTest {
 
     private DelegationSortedIntList list1;
     private DelegationSortedIntList list2;
 
-    /**
-     * Called before each test.
-     */
     @Before
     public void setUp() {
         list1 = new DelegationSortedIntList();
         list2 = new DelegationSortedIntList();
     }
 
-    /**
-     * Tests for the add() method.
-     */
     @Test
     public void testAdd() {
         list1.add(1);
@@ -31,13 +22,9 @@ public class DelegationSortedIntListTest {
         list1.add(2);
         list1.add(4);
         list1.add(2);
-
         assertEquals(5, list1.getTotalAdded());
     }
 
-    /**
-     * Tests for the addAll() method.
-     */
     @Test
     public void testAddAll() {
         list1.add(1);
@@ -57,9 +44,7 @@ public class DelegationSortedIntListTest {
     }
 
     /**
-     * A helper function that prints out the contents of an IntegerList.
-     *
-     * @param list IntegerList to be printed out.
+     * @param list 
      */
     private void printList(IntegerList list) {
         for (int i = 0; i < list.size(); i++) {
--- Java/src/test/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntListTest.java
+++ Java/src/test/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntListTest.java
@@ -14,62 +14,39 @@ public class InheritanceSortedIntListTest {
     private InheritanceSortedIntList list1;
     private InheritanceSortedIntList list2;
 
-    /**
-     * Called before each test.
-     */
     @Before
     public void setUp() {
         list1 = new InheritanceSortedIntList();
         list2 = new InheritanceSortedIntList();
     }
 
-    /**
-     * Tests for the add() method.
-     */
     @Test
     public void testAdd() {
-        // Add 5 elements to our list.
         list1.add(1);
         list1.add(3);
         list1.add(2);
         list1.add(4);
         list1.add(2);
-
-        // Check that the total number of elements added is 5.
         assertEquals(5, list1.getTotalAdded());
     }
 
-    /**
-     * Tests for the addAll() method.
-     */
     @Test
     public void testAddAll() {
-        // Add 5 elements to our first list.
         list1.add(1);
         list1.add(3);
         list1.add(2);
         list1.add(4);
         list1.add(2);
-
-        // Check that the total number of elements added is 5.
         assertEquals(5, list1.getTotalAdded());
 
-        // Add 2 elements to a second list.
         list2.add(3);
         list2.add(0);
-
-        // Check that the total number of elements added is 2.
         assertEquals(2, list2.getTotalAdded());
-
-        // Add the first list (5 elements) to our second list (2 elements).
         list2.addAll(list1);
-
-        // Check that the total number of elements added to our second list is 7.
         assertEquals(7, list2.getTotalAdded());
     }
 
     /**
-     * A helper function that prints out the contents of an IntegerList.
      * @param list IntegerList to be printed out.
      */
     private void printList(InheritanceSortedIntList list) {
